eb1c98a0d3153b854ae98bb76ab5870cc6c525d8
wzjsun <530739162@qq.com>
2022-04-15 17:31:18 +0800

    feat: desc定义为泛型而非关联类型

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@ pub use pi_res::{Res, ResMgr};
 
 /// 资源
 /// 一个需要LoadMgr加载的资源，需要实现该trait
-pub trait Asset: Res {
-	type Desc;
+pub trait Asset<Desc>: Res {
+
 
 	/// 取到资源的状态
 	fn state(&self) -> State;
@@ -24,14 +24,14 @@ pub trait Asset: Res {
     fn async_load(
 		load_mgr: &mut LoadMgr, 
 		asset: Share<Self>/* Share<ShareCell<Self>>?*/, 
-		desc: Self::Desc) -> BoxFuture<'static, ()>;
+		desc: Desc) -> BoxFuture<'static, ()>;
 	/// 同步加载资源
 	// 加载资源前，需要设置资源状态为Loading， 加载资源后，需要设置资源状态为Ok
 	// asset可能需要是Share<ShareCell<Self>>
     fn load(
 		load_mgr: &mut LoadMgr,
 		asset: Share<Self>/* Share<ShareCell<Self>>?*/, 
-		desc: Self::Desc);
+		desc: Desc);
 }
 
 /// 资源加载管理器
@@ -46,19 +46,19 @@ impl LoadMgr {
 		LoadMgr { res_mgr }
 	}
 	/// 取到资源
-	pub fn get<A: Asset>(&mut self, key: &A::Key, group: usize) -> Option<Share<A>> {
+	pub fn get<D, A: Asset<D>>(&mut self, key: &A::Key, group: usize) -> Option<Share<A>> {
 		self.res_mgr.get(key, group)
 	}
 
 	/// 创建资源
 	/// 如果get方法取不到资源，调用该方法创建一个资源
-	pub fn create<A: Asset>(&mut self, key: A::Key, group_i: usize, cost: usize, asset: A, desc: A::Desc) -> BoxFuture<'static, ()> {
+	pub fn create<D, A: Asset<D>>(&mut self, key: A::Key, group_i: usize, cost: usize, asset: A, desc: D) -> BoxFuture<'static, ()> {
 		let res = self.res_mgr.create(key, group_i, asset, cost);
 		self.load(res, desc)
 	}
 
 	// 加载资源
-	pub fn load<A: Asset>(&mut self, asset: Share<A>, desc: A::Desc) -> BoxFuture<'static, ()> {
+	pub fn load<D, A: Asset<D>>(&mut self, asset: Share<A>, desc: D) -> BoxFuture<'static, ()> {
 		match asset.state() {
 			State::Waiting => if A::is_async() {
 				A::async_load(self, asset, desc)
